a148c7f02b23a9e3574bdedded412661a9db2df6
thetlwin-git <thetlwin.dev@gmail.com>
2023-12-04 13:51:28 +0800

    feat: Added AutoDrivingCarServiceTests for unit testing AutoDrivingCarSimulation.Application
    Implemented the AutoDrivingCarServiceTests class within the AutoDrivingCarSimulation.Tests project to conduct unit tests specifically targeting the functionalities and behaviors of the AutoDrivingCarSimulation.Application layer. This test suite aims to validate the correctness and reliability of the application layer's functionalities through unit testing.

--- AutoDrivingCarSimulation.Tests/AutoDrivingCarServiceTests.cs
+++ AutoDrivingCarSimulation.Tests/AutoDrivingCarServiceTests.cs
@@ -1,16 +1,44 @@
+using AutoDrivingCarSimulation.Application;
+
 namespace AutoDrivingCarSimulation.Tests
 {
+    [TestFixture]
     public class AutoDrivingCarServiceTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void ExecuteCommands_CarMovesWithinBounds_Successfully()
         {
+            AutoDrivingCarService carService = new AutoDrivingCarService();
+            string finalPosition = carService.ExecuteCommands(5, 5, "2 2 N", "FFRFFFRRLF");
+
+            Assert.That(finalPosition, Is.EqualTo("4 3 S"));
         }
 
         [Test]
-        public void Test1()
+        public void ExecuteCommands_CarStaysWithinBounds_IgnoreOutOfBoundsCommands()
         {
-            Assert.Pass();
+            AutoDrivingCarService carService = new AutoDrivingCarService();
+            string finalPosition = carService.ExecuteCommands(3, 3, "0 0 S", "FFRF");
+
+            Assert.That(finalPosition, Is.Not.EqualTo("0 0 S"));
+        }
+
+        [Test]
+        public void ExecuteCommands_CarRotatesLeft_Successfully()
+        {
+            AutoDrivingCarService carService = new AutoDrivingCarService();
+            string finalPosition = carService.ExecuteCommands(5, 5, "2 2 N", "L");
+
+            Assert.That(finalPosition, Is.EqualTo("2 2 W"));
+        }
+
+        [Test]
+        public void ExecuteCommands_CarRotatesRight_Successfully()
+        {
+            AutoDrivingCarService carService = new AutoDrivingCarService();
+            string finalPosition = carService.ExecuteCommands(5, 5, "2 2 N", "R");
+
+            Assert.That(finalPosition, Is.EqualTo("2 2 E"));
         }
     }
 }
