b6d4a29a42e4ba76f43151c93c6414909879c675
thetlwin-git <thetlwin.dev@gmail.com>
2023-12-04 13:35:31 +0800

    feat: Implemented ExecuteCommands method for auto driving car simulation
    Added the ExecuteCommands method within the AutoDrivingCarService class to handle and execute commands for the auto driving car simulation. This method orchestrates the execution of commands, enabling the car to perform movements and update its position based on the provided instructions.

--- AutoDrivingCarSimulation.Application/AutoDrivingCarService.cs
+++ AutoDrivingCarSimulation.Application/AutoDrivingCarService.cs
@@ -1,7 +1,20 @@
-﻿namespace AutoDrivingCarSimulation.Application
+﻿using AutoDrivingCarSimulation.Domain;
+
+namespace AutoDrivingCarSimulation.Application
 {
     public class AutoDrivingCarService
     {
+        public string ExecuteCommands(int width, int height, string initialPosition, string commands)
+        {
+            string[] initialPositionDetails = initialPosition.Split(' ');
+            int x = int.Parse(initialPositionDetails[0]);
+            int y = int.Parse(initialPositionDetails[1]);
+            char direction = char.Parse(initialPositionDetails[2]);
 
+            AutoDrivingCar car = new AutoDrivingCar(width, height, x, y, direction);
+            car.Move(commands);
+
+            return car.GetPosition();
+        }
     }
 }
