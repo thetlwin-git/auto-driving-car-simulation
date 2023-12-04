c95c982c4f7664d4a168bc878e137ed69e8ec713
thetlwin-git <thetlwin.dev@gmail.com>
2023-12-04 13:45:14 +0800

    feat: Initialized AutoDrivingCarSimulation.Tests for NUnit-based unit testing
    Added the AutoDrivingCarSimulation.Tests project to the solution, enabling the implementation of unit tests for the auto driving car simulation. This project utilizes the NUnit framework to conduct unit testing, ensuring the validation of the application's functionalities and behaviors.

---
+++ AutoDrivingCarSimulation.Tests/AutoDrivingCarServiceTests.cs
@@ -0,0 +1,16 @@
+namespace AutoDrivingCarSimulation.Tests
+{
+    public class AutoDrivingCarServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
